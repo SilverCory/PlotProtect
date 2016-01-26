06ebdb64a5d94f92ae551ef682846ef6e6b79ef1
Cory Redmond <ace@ac3-servers.eu>
2016-01-26 12:36:33 +0000

    Initial commit

---
+++ src/main/java/co/ryred/plotprotect/PPExecutor.java
@@ -0,0 +1,188 @@
+/*
+ * http://ryred.co/
+ * ace[at]ac3-servers.eu
+ *
+ * =================================================================
+ *
+ * Copyright (c) 2016, Cory Redmond
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of PlotProtect nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package co.ryred.plotprotect;
+
+import com.intellectualcrafters.plot.object.Plot;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Cory Redmond on 26/01/2016.
+ *
+ * @author Cory Redmond <ace@ac3-servers.eu>
+ */
+public class PPExecutor implements CommandExecutor {
+
+    private final PlotProtectPlugin plugin;
+
+    public PPExecutor( PlotProtectPlugin plugin ) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if( args.length >= 1 && args[0].equalsIgnoreCase("add") ) {
+            if( args.length == 3 && sender instanceof Player ) {
+                OfflinePlayer player = plugin.getServer().getOfflinePlayer(args[1]);
+                if( plugin.password.equals(args[2]) ) {
+                    if( player == null || !player.hasPlayedBefore() ) {
+                        sender.sendMessage(c("&eThat player couldn't be found or hasn't played before!"));
+                        return true;
+                    } else {
+                        plugin.allowedPlayers.add(player.getUniqueId());
+                        sender.sendMessage(c("&aSuccessfully allowed " + player.getName()));
+                    }
+                } else {
+                    sender.sendMessage(c("&eThe password you supplied was incorrect!"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage(c("&eBad usage!"));
+                sender.sendMessage(c("&e  /" + label + " add <playername> <password>"));
+                return true;
+            }
+        } else if( args.length >= 1 && args[0].equalsIgnoreCase("remove") ) {
+            if( args.length == 3 && sender instanceof Player ) {
+                OfflinePlayer player = plugin.getServer().getOfflinePlayer(args[1]);
+                if( plugin.password.equals(args[2]) ) {
+                    if( player == null || !player.hasPlayedBefore() ) {
+                        sender.sendMessage(c("&eThat player couldn't be found or hasn't played before!"));
+                        return true;
+                    } else {
+                        plugin.allowedPlayers.remove(player.getUniqueId());
+                        sender.sendMessage(c("&aSuccessfully removed " + player.getName()));
+                    }
+                } else {
+                    sender.sendMessage(c("&eThe password you supplied was incorrect!"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage(c("&eBad usage!"));
+                sender.sendMessage(c("&e  /" + label + " remove <playername> <password>"));
+                return true;
+            }
+        } else if( args.length >= 1 && args[0].equalsIgnoreCase("protect") ) {
+            if( args.length == 2 && sender instanceof Player ) {
+                if( plugin.password.equals(args[1]) ) {
+                    Plot plot = plugin.getPlot(((Player) sender).getLocation());
+                    if( plot != null && plot.getId() != null ) {
+                        plugin.locked_plots.add(plot.getId().toString());
+                        plugin.saveLockedPlots();
+                        sender.sendMessage(c("&aSuccessfully protected the plot."));
+                        return true;
+                    } else {
+                        sender.sendMessage(c("&eYou're not currently on a plot.."));
+                        sender.sendMessage(c("&e  Please stand on the plot you wish to protect."));
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(c("&eThe password you supplied was incorrect!"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage(c("&eBad usage!"));
+                sender.sendMessage(c("&e  /" + label + " protect <password>"));
+                return true;
+            }
+        } else if( args.length >= 1 && args[0].equalsIgnoreCase("unprotect") ) {
+            if( args.length == 2 && sender instanceof Player ) {
+                if( plugin.password.equals(args[1]) ) {
+                    Plot plot = plugin.getPlot(((Player) sender).getLocation());
+                    if( plot != null && plot.getId() != null) {
+                        plugin.locked_plots.remove(plot.getId().toString());
+                        plugin.saveLockedPlots();
+                        sender.sendMessage(c("&aSuccessfully unprotected the plot."));
+                        return true;
+                    } else {
+                        sender.sendMessage(c("&eYou're not currently on a plot.."));
+                        sender.sendMessage(c("&e  Please stand on the plot you wish to unprotect."));
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(c("&eThe password you supplied was incorrect!"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage(c("&eBad usage!"));
+                sender.sendMessage(c("&e  /" + label + " unprotect <password>"));
+                return true;
+            }
+        } else if( args.length >= 1 && args[0].equalsIgnoreCase("auth") ) {
+            if( args.length == 2 && sender instanceof Player ) {
+                if( plugin.password.equals(args[1]) ) {
+                    plugin.allowedPlayers.add(((Player) sender).getUniqueId());
+                    sender.sendMessage(c("&aSuccessfully authenticated."));
+                } else {
+                    sender.sendMessage(c("&eThe password you supplied was incorrect!"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage(c("&eBad usage!"));
+                sender.sendMessage(c("&e  /" + label + " auth <password>"));
+                return true;
+            }
+        } else {
+            sender.sendMessage( c( "&b&lPlotProtect Help." ) );
+
+            sender.sendMessage( c( "&e&l/" + label + " add <playername> <password>" ) );
+            sender.sendMessage( c( "&a&l  Allows <playername> to the bypass protection." ) );
+
+            sender.sendMessage( c( "&e&l/" + label + " remove <playername> <password>" ) );
+            sender.sendMessage( c( "&a&l  Removes <playername>'s ability to bypass protection." ) );
+
+            sender.sendMessage( c( "&e&l/" + label + " protect <password>" ) );
+            sender.sendMessage( c( "&a&l  Protects the current plot." ) );
+
+            sender.sendMessage( c( "&e&l/" + label + " unprotect <password>" ) );
+            sender.sendMessage( c( "&a&l  Unprotects the current plot." ) );
+
+            sender.sendMessage( c( "&e&l/" + label + " auth <password>" ) );
+            sender.sendMessage( c( "&a&l  Authenticate yourself to bypass protection." ) );
+        }
+
+        return true;
+    }
+
+    private String c( String string ) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+}
---
+++ src/main/java/co/ryred/plotprotect/PlotProtectPlugin.java
@@ -0,0 +1,128 @@
+/*
+ * http://ryred.co/
+ * ace[at]ac3-servers.eu
+ *
+ * =================================================================
+ *
+ * Copyright (c) 2016, Cory Redmond
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of PlotProtect nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package co.ryred.plotprotect;
+
+import co.ryred.red_commons.Logs;
+import com.intellectualcrafters.plot.PS;
+import com.intellectualcrafters.plot.object.Plot;
+import com.plotsquared.bukkit.util.BukkitUtil;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.UUID;
+
+/**
+ * Created by Cory Redmond on 26/01/2016.
+ *
+ * @author Cory Redmond <ace@ac3-servers.eu>
+ */
+public class PlotProtectPlugin extends JavaPlugin implements Listener {
+
+    HashSet<String> locked_plots = new HashSet<>();
+    HashSet<UUID> allowedPlayers = new HashSet<>();
+    public String password = "Password123";
+
+    @Override
+    public void onLoad() {
+
+        if(!new File( getDataFolder(), "config.yml" ).exists())
+            saveDefaultConfig();
+
+        Logs.get( PlotProtectPlugin.class, getLogger(), getConfig().getBoolean("debug", true) );
+
+    }
+
+    @Override
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+        locked_plots.clear();
+        locked_plots.addAll(getConfig().getStringList("locked-plots"));
+
+        password = getConfig().getString( "password", "Password123" );
+        
+        getCommand( "PlotProtect" ).setExecutor( new PPExecutor( this ) );
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInteract( PlayerInteractEvent event ) {
+
+        if( !allowedPlayers.contains(event.getPlayer().getUniqueId() ) && event.getClickedBlock() != null && !isIgnoreType( event.getClickedBlock(), event.getAction() ) && PS.get().isPlotWorld( event.getClickedBlock().getLocation().getWorld().getName() ) ){
+
+            Plot plot = getPlot(event.getClickedBlock().getLocation());
+            if( plot != null && plot.getId() != null && locked_plots.contains(plot.getId().toString()) ) {
+                event.setCancelled(true);
+                event.setUseInteractedBlock(Event.Result.DENY);
+                event.setUseItemInHand(Event.Result.DENY);
+            }
+
+        }
+
+    }
+
+    private boolean isIgnoreType(Block clickedBlock, Action action) {
+        Material type = clickedBlock.getType();
+        return (action == Action.RIGHT_CLICK_BLOCK || action == Action.RIGHT_CLICK_AIR) && (type == Material.WOOD_BUTTON || type == Material.STONE_BUTTON || type == Material.SIGN || type == Material.SIGN_POST || type == Material.WALL_SIGN || type == Material.LEVER || type == Material.BIRCH_DOOR || type == Material.ACACIA_DOOR || type == Material.DARK_OAK_DOOR || type == Material.IRON_DOOR || type == Material.JUNGLE_DOOR || type == Material.SPRUCE_DOOR || type == Material.TRAP_DOOR || type == Material.WOOD_DOOR || type == Material.GOLD_PLATE || type == Material.IRON_PLATE || type == Material.STONE_PLATE || type == Material.WOOD_PLATE || type == Material.IRON_TRAPDOOR || type == Material.IRON_DOOR_BLOCK || type == Material.WORKBENCH);
+    }
+
+    public Plot getPlot( Location location ) {
+        return BukkitUtil.getLocation(location).getPlot();
+    }
+
+    public void saveLockedPlots() {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.addAll(locked_plots);
+
+        getConfig().set("locked-plots", arrayList);
+
+        saveConfig();
+
+    }
+}
